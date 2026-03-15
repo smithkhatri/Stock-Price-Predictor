ba518eaeeb1c310232c7f6d427c52eac5d093463
Smith Khatri <smithkhatri9@gmail.com>
2026-03-15 13:42:38 -0400

    Prepare data done

---
+++ prepare_data.py
@@ -0,0 +1,24 @@
+import pandas as pd
+
+import numpy as np
+
+data = pd.read_csv("AAPL.csv")
+
+close = data["Close"].values
+
+close = close[2:]
+
+close = close.astype("float")
+
+X = []
+y = []
+
+sliding_window_size = 10
+
+for i in range(sliding_window_size, len(close)):
+    y.append(close[i])
+    X.append(close[i-sliding_window_size:i])
+
+
+X = np.array(X)
+y = np.array(y)
