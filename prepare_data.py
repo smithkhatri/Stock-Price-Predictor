ea7303a66aa0b87d8ac47214f221c704cf9b4e71
Smith Khatri <smithkhatri9@gmail.com>
2026-03-15 17:36:01 -0400

    Prepare data done

--- prepare_data.py
+++ prepare_data.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-import numpy as np
+from sklearn.preprocessing import MinMaxScaler
 
 data = pd.read_csv("AAPL.csv")
 
@@ -13,12 +13,21 @@ close = close.astype("float")
 X = []
 y = []
 
+close = close.reshape(-1, 1)
+
+scaler = MinMaxScaler()
+
+close = scaler.fit_transform(close)
+
+
 sliding_window_size = 10
 
 for i in range(sliding_window_size, len(close)):
-    y.append(close[i])
+    y.append(close[i][0])
     X.append(close[i-sliding_window_size:i])
 
 
+import numpy as np
 X = np.array(X)
 y = np.array(y)
+
